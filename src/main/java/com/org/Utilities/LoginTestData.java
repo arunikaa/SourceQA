5ab81f0e0cf3d6017cd7119cbd7f481992343e22
Arunika <110450170+arunikaa@users.noreply.github.com>
2026-04-20 12:50:38 +1200

    commit the error handling and coding best practices_20_04_2026

--- src/main/java/com/org/Utilities/LoginTestData.java
+++ src/main/java/com/org/Utilities/LoginTestData.java
@@ -1,13 +1,9 @@
 package com.org.Utilities;
 
 public class LoginTestData {
-    private final String username;
-    private final String password;
+    private  String username;
+    private  String password;
 
-    public LoginTestData(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 
     public String getUsername() { return username; }
     public String getPassword() { return password; }
